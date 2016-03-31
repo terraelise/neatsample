c3cf6080fa5e0f34d4ff1a6180e1d504346b8a12
Terra E. Whitmeyer <terra.whitmeyer@vividdream.net>
2016-03-31 08:02:30 -0700

    start header, add carousel and most styling

--- README.md
+++ README.md
@@ -2,6 +2,6 @@
 
 see dist/index.html
 
-still to do: header, main carousel
+still to do: header nav & twitter
 
 Props to cheat sheets for install/setup: http://blog.meredithunderell.com/gulp-sass-bourbon-neat-browsersync-boilerplate/ and https://laravel-news.com/2014/03/using-bourbon-neat-with-gulp/
---
+++ dist/js/script.js
@@ -0,0 +1,11 @@
+$(document).ready(function(){
+	$("#intro").owlCarousel({
+	    items: 1,
+	    margin: 0,
+	    loop: true,
+	    autoplay: false,
+	    autoplayHoverPause: true,
+	    nav: true,
+	    navText: ["<span class='chevron chevron-left'></span>","<span class='chevron chevron-right'></span>"]
+	});
+});
